45eec418f45717aaf3c33a546beb5f3ef223db88
Login Victor <batazor111@gmail.com>
2017-11-04 20:15:56 +0300

    Refactoring: add pages

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Link } from 'react-router-dom'
-import Home from '../home'
-import About from '../about'
+import Home from '../Home'
+import About from '../About'
 
 const App = () => (
   <div>
