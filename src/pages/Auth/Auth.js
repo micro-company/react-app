cecf32c551c033135ca023f158f5385618975886
Login Victor <batazor111@gmail.com>
2017-11-05 03:07:04 +0300

    Add Auth component

--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -1,11 +1,62 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from 'material-ui/styles'
 import { Helmet } from 'react-helmet'
+import Grid from 'material-ui/Grid'
+import Typography from 'material-ui/Typography'
+import Button from 'material-ui/Button'
+import Card, { CardActions, CardContent } from 'material-ui/Card'
 
-export default () => [
-  <Helmet title="Auth" />,
+const styles = () => ({
+  root: {
+    flexGrow: 1,
+    height: '100%',
+  },
+  button: {
+    flex: 1,
+  },
+})
 
-  <div key="content">
-    <h1>Auth Us</h1>
-    <p>Hello Medium!</p>
-  </div>,
-]
+class Auth extends PureComponent {
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+  }
+
+  render() {
+    const { classes } = this.props
+
+    return ([
+      <Helmet title="Auth" />,
+
+      <Grid container className={classes.root} key="content" alignItems="center">
+        <Grid item xs={12}>
+          <Grid container justify="center">
+            <Grid item xs={3}>
+              <Card>
+                <CardContent>
+                  <Typography type="display1" gutterBottom>
+                    Auth Us
+                  </Typography>
+                </CardContent>
+
+                <CardActions>
+                  <Grid container>
+                    <Button raised color="primary" className={classes.button}>
+                      Sigh In
+                    </Button>
+
+                    <Button raised color="primary" className={classes.button}>
+                      Sign on
+                    </Button>
+                  </Grid>
+                </CardActions>
+              </Card>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>,
+    ])
+  }
+}
+
+export default withStyles(styles)(Auth)
