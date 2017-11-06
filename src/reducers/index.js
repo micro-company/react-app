2c6269c49c7434bb2cc9d9a0d46cf5be4d4a281e
Login Victor <batazor111@gmail.com>
2017-11-06 22:19:39 +0300

    Enable redux-form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import { routerReducer as router } from 'react-router-redux'
+import { reducer as formReducer } from 'redux-form'
 import counter from './counter'
 import session from './session'
 
 export default combineReducers({
   router,
+  form: formReducer,
 
   session,
   counter,
