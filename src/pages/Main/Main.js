8d84ec2f5ab55317cff0ec43c0ac42ead0d71135
Login Victor <batazor111@gmail.com>
2017-11-12 00:07:26 +0300

    Update style Main page

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import { Route, Link } from 'react-router-dom'
+import Grid from 'material-ui/Grid'
 import Home from '../Home'
 import About from '../About'
 
-export default () => [
-  <header key="header">
-    <Link to="/">Home</Link>
-    <Link to="/about-us">About</Link>
-  </header>,
+export default () => (
+  <Grid container>
+    <Grid item xs={12}>
+      <header key="header">
+        <Link to="/">Home</Link>
+        <Link to="/about-us">About</Link>
+      </header>
+    </Grid>
 
-  <main key="main">
-    <Route exact path="/" component={Home} />
-    <Route exact path="/about-us" component={About} />
-  </main>,
-]
+    <Grid item xs={12}>
+      <main key="main">
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about-us" component={About} />
+      </main>
+    </Grid>
+  </Grid>
+)
