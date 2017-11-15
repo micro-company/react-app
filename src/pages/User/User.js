5661e2589d14cbf95629f174d519b3fb017c4199
Login Victor <batazor111@gmail.com>
2017-11-16 01:08:04 +0300

    Update user page layout

--- src/pages/User/User.js
+++ src/pages/User/User.js
@@ -1,19 +1,62 @@
 import React, { PureComponent } from 'react'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 // import { bindActionCreators } from 'redux'
+import { withStyles } from 'material-ui/styles'
 import { connect } from 'react-redux'
 import { Helmet } from 'react-helmet'
+import ReactTable from 'react-table'
+import Button from 'material-ui/Button'
+import Grid from 'material-ui/Grid'
+import Typography from 'material-ui/Typography'
+import AddIcon from 'material-ui-icons/Add'
+
+const styles = () => ({
+  header: {
+    display: 'flex',
+  },
+  add: {
+    flex: 1,
+  },
+})
 
 class User extends PureComponent {
-  static propTypes = {}
+  static propTypes = {
+    classes: PropTypes.string.isRequired,
+  }
 
   render() {
+    const { classes } = this.props
+
     return [
       <Helmet key="user" title="User" />,
 
-      <div key="content">
-        <h1>User</h1>
-      </div>,
+      <Grid container direction="column" key="content">
+        <Grid item>
+          <Grid container direction="row" key="content">
+            <Typography type="display1" gutterBottom>
+              Users list
+            </Typography>
+
+            <div className={classes.add} />
+
+            <Button fab color="primary" aria-label="add">
+              <AddIcon />
+            </Button>
+          </Grid>
+        </Grid>
+
+        <Grid item>
+          <ReactTable
+            data={[]}
+            columns={[
+              {
+                Header: 'Mail',
+                accessor: 'mail',
+              },
+            ]}
+          />
+        </Grid>
+      </Grid>,
     ]
   }
 }
@@ -29,4 +72,4 @@ function mapDispatchToProps() {
 export default connect(
   mapStateToProps,
   mapDispatchToProps,
-)(User)
+)(withStyles(styles)(User))
