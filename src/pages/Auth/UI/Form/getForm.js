fec7d6348ad67d5b2bcb2254c5d9efd8dadf4a3f
Login Victor <batazor111@gmail.com>
2018-03-27 14:12:44 +0300

    Improve login form

--- src/pages/Auth/UI/Form/getForm.js
+++ src/pages/Auth/UI/Form/getForm.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Field } from 'redux-form'
 import { MenuItem } from 'material-ui/Menu'
 import { TextField, Select } from 'redux-form-material-ui'
+import { FormControl } from 'material-ui/Form'
+import { InputLabel } from 'material-ui/Input'
 
 export default function getForm(currentTab) {
   switch (currentTab) {
@@ -55,15 +57,18 @@ export default function getForm(currentTab) {
           component={TextField}
           fullWidth
         />,
-        <Field
-          key="language"
-          name="language"
-          component={Select}
-          fullWidth
-        >
-          <MenuItem value="ru_RU">Russia</MenuItem>
-          <MenuItem value="en_GB">English</MenuItem>
-        </Field>,
+        <FormControl style={{ width: '100%' }}>
+          <InputLabel htmlFor="controlled-open-select">Language</InputLabel>
+          <Field
+            key="language"
+            name="language"
+            component={Select}
+            fullWidth
+          >
+            <MenuItem value="ru_RU">Russia</MenuItem>
+            <MenuItem value="en_GB">English</MenuItem>
+          </Field>
+        </FormControl>,
       ]
     case 'logIn':
     default:
@@ -75,7 +80,6 @@ export default function getForm(currentTab) {
           component={TextField}
           fullWidth
         />,
-
         <Field
           key="password"
           name="password"
@@ -84,16 +88,18 @@ export default function getForm(currentTab) {
           component={TextField}
           fullWidth
         />,
-
-        <Field
-          key="language"
-          name="language"
-          component={Select}
-          fullWidth
-        >
-          <MenuItem value="ru_RU">Russia</MenuItem>
-          <MenuItem value="en_GB">English</MenuItem>
-        </Field>,
+        <FormControl style={{ width: '100%' }}>
+          <InputLabel htmlFor="controlled-open-select">Language</InputLabel>
+          <Field
+            key="language"
+            name="language"
+            component={Select}
+            fullWidth
+          >
+            <MenuItem value="ru_RU">Russia</MenuItem>
+            <MenuItem value="en_GB">English</MenuItem>
+          </Field>
+        </FormControl>,
       ]
   }
 }
